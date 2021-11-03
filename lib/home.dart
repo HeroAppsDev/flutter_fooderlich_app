54131ad1e3f50d74121ed2e839f661fc80a2665c
Kenneth Terrago <kterrago091893@gmail.com>
2021-11-03 11:27:04 +0800

    :zap: Add bottom navigation bar with three screens

---
+++ lib/home.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fooderlich_app/card2.dart';
+import 'package:flutter_fooderlich_app/card3.dart';
+
+import 'card1.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = [
+    const Card1(),
+    const Card2(),
+    const Card3(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'FooderLich',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card 2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card 3',
+          ),
+        ],
+      ),
+    );
+  }
+}
