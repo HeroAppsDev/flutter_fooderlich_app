b8e298dc56a3937acd88663dc7d4c78aeed378da
Kenneth Terrago <kterrago091893@gmail.com>
2021-11-15 14:47:22 +0800

    :sparkles: Adding state listeners

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -7,6 +7,19 @@ class AppRouter extends RouterDelegate
   late final GroceryManager groceryManager;
   late final ProfileManager profileManager;
 
+  AppRouter({
+    required this.appStateManager,
+    required this.groceryManager,
+    required this.profileManager,
+  }) : navigatorKey = GlobalKey<NavigatorState>() {
+    appStateManager.addListener(
+        notifyListeners); //Determines the state of the app. It manages whether the app initialized login and if the user completed the onboarding.
+    groceryManager.addListener(
+        notifyListeners); // Manages the list of grocery items and the item selection state.
+    profileManager.addListener(
+        notifyListeners); // Manages the user’s profile and settings.
+  }
+
   @override
   Widget build(BuildContext context) {
     throw Navigator(
@@ -24,18 +37,21 @@ class AppRouter extends RouterDelegate
   }
 
   @override
-  final GlobalKey<NavigatorState> navigatorKey;
-  //GlobalKey - a unique key across the entire app.
+  final GlobalKey<NavigatorState>
+      navigatorKey; //GlobalKey - a unique key across the entire app.
+
+  //Sets setNewRoutePath to null since you aren’t supporting Flutter web apps yet.
 
   @override
   Future<void> setNewRoutePath(configuration) async {
     return;
   }
-  //Sets setNewRoutePath to null since you aren’t supporting Flutter web apps yet.
 
-  AppRouter({
-    required this.appStateManager,
-    required this.groceryManager,
-    required this.profileManager,
-  }) : navigatorKey = GlobalKey<NavigatorState>() {}
+  @override
+  void dispose() {
+    appStateManager.removeListener(notifyListeners);
+    groceryManager.removeListener(notifyListeners);
+    profileManager.removeListener(notifyListeners);
+    super.dispose();
+  }
 }
